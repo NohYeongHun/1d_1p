d35fa9bb432a51a7fcff627b213b13db4c12c2b2
ny <dkzndk1100@naver.com>
2023-01-17 21:40:22 +0900

    7576.py

---
+++ 7576.py
@@ -0,0 +1,61 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+def check(board):
+    n = len(board)
+    m = len(board[0])
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 0:
+                return False
+    return True
+
+
+def solve(q, board, visited):
+    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    temp = []
+    while q:
+        r, c = q.pop()
+        for i in range(4):
+            dr, dc = r + dx[i], c + dy[i]
+
+            if dr < 0 or dr >= n or dc < 0 or dc >= m:
+                continue
+            if visited[dr][dc]:
+                continue
+            if board[dr][dc] == 0:
+                board[dr][dc] = 1
+                visited[dr][dc] = True
+                temp.append([dr, dc])
+    return temp
+
+
+if __name__ == "__main__":
+    m, n = map(int, input().split())
+    board = []
+    for _ in range(n):
+        arr = list(map(int, input().split()))
+        board.append(arr)
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    q = []
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 1:
+                q.append([i, j])
+                visited[i][j] = True
+
+    for cnt in range(1_000_000):
+        q = solve(q, board, visited)
+
+        if not q:
+            if check(board):
+                print(cnt)
+                exit(0)
+            else:
+                print(-1)
+                exit(0)
