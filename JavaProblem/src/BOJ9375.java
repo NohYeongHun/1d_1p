568e3c88e765e91ce6b14acc32d43b27aa003f3b
 NohYeongHun <dkzndk1100@naver.com>
2023-01-29 14:21:24 +0900

    JavaProject 생성

--- JavaProblem/src/BOJ9375.java
+++ JavaProblem/src/BOJ9375.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-public class Main {
+public class BOJ9375 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Integer T = Integer.parseInt(br.readLine());
